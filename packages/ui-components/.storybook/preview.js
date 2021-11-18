63b7d0da333e7c3918968b9f2ca2f8a21541b19a
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-18 09:48:58 +0100

    added backgrounds to storybook

--- packages/ui-components/.storybook/preview.js
+++ packages/ui-components/.storybook/preview.js
@@ -2,4 +2,21 @@ import '../src/styles/tailwind.css';
 
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
+  backgrounds: {
+    default: 'neutral-50',
+    values: [
+      {
+        name: 'neutral-50',
+        value: '#F5F8FF',
+      },
+      {
+        name: 'white',
+        value: '#fff',
+      },
+      {
+        name: 'neutral-900',
+        value: '#1F2933',
+      },
+    ],
+  },
 };
