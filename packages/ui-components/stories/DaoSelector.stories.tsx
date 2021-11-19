00467a8773f4e06e67efa25bc69d6365bfd97dd1
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-19 18:08:58 +0100

    verification commit

--- packages/ui-components/stories/DaoSelector.stories.tsx
+++ packages/ui-components/stories/DaoSelector.stories.tsx
@@ -13,7 +13,7 @@ export const Default = Template.bind({});
 Default.args = {
   src:
     'https://banner2.cleanpng.com/20180325/sxw/kisspng-computer-icons-avatar-avatar-5ab7529a8e4e14.9936310115219636745829.jpg',
-  label: 'DAO Name',
+  label: 'My DAO',
   onClick: () => {
     alert('hey, you just triggered the onClick method :)');
   },
@@ -23,7 +23,7 @@ export const Selected = Template.bind({});
 Selected.args = {
   src:
     'https://banner2.cleanpng.com/20180325/sxw/kisspng-computer-icons-avatar-avatar-5ab7529a8e4e14.9936310115219636745829.jpg',
-  label: 'DAO Name',
+  label: 'My DAO',
   onClick: () => {
     alert('hey, you just triggered the onClick method :)');
   },
