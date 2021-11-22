480b40aaf619a5e2b9db4824b5542eb7c4191c0e
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-22 15:15:20 +0100

    daoCard doumentation

--- packages/ui-components/src/components/daoCard/daoCard.tsx
+++ packages/ui-components/src/components/daoCard/daoCard.tsx
@@ -9,10 +9,22 @@ import {IconSwitch} from '../icons';
 
 export type DaoCardProps = {
   daoName: string;
+  /** Dao's ethereum address **or** ENS name */
   daoAddress: Address;
+  /** Handler for the switch button. Will be called when the button is clicked. */
   onClick: () => void;
 } & Pick<AvatarProps, 'src'>;
 
+/**
+ * ## Description
+ *
+ * This card is used to display the current DAO (including name and (ens-)
+ * address). Additionally, the switch button allows a user to access the DAO
+ * explorer, where they can change DAO.
+ *
+ * > Note: This component is currently only designed for mobile. It may still be
+ * > used for larger devices, though.
+ */
 export const DaoCard: React.FC<DaoCardProps> = ({
   daoName,
   daoAddress,
