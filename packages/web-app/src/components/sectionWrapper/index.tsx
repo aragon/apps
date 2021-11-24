155f793b7fd904e736081db6ad386db3fb0317e6
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-24 18:17:04 +0100

    token and transfer section wrapper

--- packages/web-app/src/components/sectionWrapper/index.tsx
+++ packages/web-app/src/components/sectionWrapper/index.tsx
@@ -1,6 +1,52 @@
-export * from './tokenSection';
+import React from 'react';
+import styled from 'styled-components';
+
+import {Button, IconButton, IconLinkExternal} from '@aragon/ui-components';
 
 export type SectionWrapperProps = {
   title: string;
   children: React.ReactNode;
 };
+
+export const TokenSectionWrapper = ({title, children}: SectionWrapperProps) => {
+  return (
+    <>
+      <HeaderContainer>
+        <Title>{title}</Title>
+        <IconButton
+          mode="ghost"
+          size="small"
+          side="right"
+          label={'See on Explorer'}
+          icon={<IconLinkExternal />}
+          onClick={() => (window.location.href = 'google.com')}
+        />
+      </HeaderContainer>
+      {children}
+      <Button mode={'ghost'} label={'See all'} />
+    </>
+  );
+};
+
+export const TransferSectionWrapper = ({
+  title,
+  children,
+}: SectionWrapperProps) => {
+  return (
+    <>
+      <HeaderContainer>
+        <Title>{title}</Title>
+      </HeaderContainer>
+      {children}
+      <Button mode={'ghost'} label={'See all'} />
+    </>
+  );
+};
+
+const Title = styled.p.attrs({
+  className: 'text-lg font-bold',
+})``;
+
+const HeaderContainer = styled.div.attrs({
+  className: 'flex justify-between',
+})``;
