7942e64a11f5a2768b7b45598173e32f025d376d
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-26 13:16:01 +0100

    added documentation

--- packages/web-app/src/components/sectionWrapper/index.tsx
+++ packages/web-app/src/components/sectionWrapper/index.tsx
@@ -10,6 +10,18 @@ export type SectionWrapperProps = {
   children: React.ReactNode;
 };
 
+// NOTE: It's possible to merge these two components. But I'm not sure it makes
+// things any simpler right now. However, if other sections wrappers like these
+// are added in the future and all have similar style, feel free to merge them.
+
+/**
+ * Section wrapper for tokens overview. Consists of a header with a title and a
+ * button, as well as a footer with a button that takes the user to the token
+ * overview. and a list of tokens (the children).
+ *
+ * NOTE: The wrapper imposes NO SPACING. It's entirely up to the children to
+ * define this.
+ */
 export const TokenSectionWrapper = ({title, children}: SectionWrapperProps) => {
   return (
     <>
@@ -30,6 +42,14 @@ export const TokenSectionWrapper = ({title, children}: SectionWrapperProps) => {
   );
 };
 
+/**
+ * Section wrapper for transfer overview. Consists of a header with a title, as
+ * well as a footer with a button that takes the user to the token overview. and
+ * a list of transfers (the children).
+ *
+ * NOTE: The wrapper imposes NO SPACING. It's entirely up to the children to
+ * define this.
+ */
 export const TransferSectionWrapper = ({
   title,
   children,
