073a9e6e5cbaffb2c0de7d057386de72c5498bfd
Fabrice Francois <Fabricevladimir@gmail.com>
2021-11-30 09:27:19 -0500

    fixed tests

--- packages/ui-components/stories/transferListItem.stories.tsx
+++ packages/ui-components/stories/transferListItem.stories.tsx
@@ -14,5 +14,18 @@ const Template: Story<TransferListItemProps> = args => (
 export const Default = Template.bind({});
 Default.args = {
   title: 'Deposit',
-  transferDate: '',
+  transferDate: 'Yesterday',
+  tokenAmount: 300,
+  tokenSymbol: 'DAI',
+  usdValue: '$200.00',
+};
+
+export const Pending = Template.bind({});
+Pending.args = {
+  isPending: true,
+  title: 'Deposit',
+  transferDate: 'Pending...',
+  tokenAmount: 300,
+  tokenSymbol: 'DAI',
+  usdValue: '$200.00',
 };
