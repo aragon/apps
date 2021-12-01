11d5fcd8b69322fd04a572423d599e37e51cb813
Fabrice Francois <Fabricevladimir@gmail.com>
2021-12-01 08:56:29 -0500

    hooks for data fetching

--- packages/web-app/src/pages/finance.tsx
+++ packages/web-app/src/pages/finance.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import {constants} from 'ethers';
+
+import usePollTokens from 'hooks/usePollTokens';
+
+const TEMP_FETCH_INTERVAL = 300000;
+const STATIC_TOKEN_ADDRESSES = [
+  constants.AddressZero, // ETH, Note: ETH will also work
+  '0xa117000000f279d81a1d3cc75430faa017fa5a2e', // ANT
+  '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
+  '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI
+  '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
+];
 
 const Finance: React.FC = () => {
+  const {isLoading, prices} = usePollTokens(
+    STATIC_TOKEN_ADDRESSES,
+    TEMP_FETCH_INTERVAL
+  );
+
+  !isLoading && console.log(prices);
   return <h1>Finance Page</h1>;
 };
 
--- packages/web-app/src/services/prices.ts
+++ packages/web-app/src/services/prices.ts
@@ -1,9 +1,11 @@
+import {constants} from 'ethers';
+
 const BASE_URL = 'https://api.0x.org';
 const BUY_TOKEN = 'USDC';
 const DEFAULT_SELL_AMOUNT = '1000000000000000000';
 
 /**
- * Get the base unit
+ * Get the base unit of a token
  * @param decimals - ERC20 token decimals
  * @returns One unit of the token
  */
@@ -16,15 +18,23 @@ const getSellAmount = (decimals?: number | string) => {
   else throw new TypeError('decimals must be a valid whole number');
 };
 
+/**
+ * Fetch the price of a given token
+ * Note: currently gas prices are NOT factored in
+ * @param token Token contract address or token symbol
+ * @param decimals Token decimals; defaults to 18
+ * @returns The approximate USD value (based on USDC) for given token
+ */
 const fetchTokenUsdPrice = async (
   token: string,
   decimals?: string | number
 ) => {
+  const sellToken = token === constants.AddressZero ? 'ETH' : token;
   try {
     const response = await fetch(
       `${BASE_URL}/swap/v1/price?sellAmount=${getSellAmount(
         decimals
-      )}&sellToken=${token}&buyToken=${BUY_TOKEN}`
+      )}&sellToken=${sellToken}&buyToken=${BUY_TOKEN}`
     );
 
     const {price} = await response.json();
