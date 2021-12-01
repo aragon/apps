a6f5432d225d6e3c0c2d93c5b2fcb2c0510e4a26
Sepehr sanaee <sepehr_sanaee78@yahoo.com>
2021-12-01 23:51:24 +0330

    IconTextInput & searchInput added

---
+++ packages/ui-components/src/components/input/SearchInput.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {TextInput, TextInputProps} from './textInput';
+import {IconSearch} from '../icons';
+
+/** Simple input with variable styling (depending on mode) */
+
+export type SearchInputProps = Omit<TextInputProps, 'adornment' | 'side'>;
+
+export const SearchInput: React.FC<SearchInputProps> = ({
+  ...props
+}) => {
+  return (
+    <TextInput
+      {...props} 
+      adornment={
+        <IconSearch className="text-ui-300"/>
+      } 
+      side={'left'}
+    />
+  );
+};
---
+++ packages/ui-components/src/components/input/textInput.tsx
@@ -0,0 +1,67 @@
+import React, {ReactNode} from 'react';
+import styled from 'styled-components';
+
+export type TextInputProps = {
+  /** Changes a input's color schema */
+  mode?: 'default' | 'success' | 'warning' | 'critical';
+   /**
+   * Whether Input is disabled
+   */
+  disabled?: boolean;
+   /**
+   * Whether Input is disabled
+   */
+  isSelected?: boolean;
+  /**
+   * adornment
+   */
+  adornment?: ReactNode;
+  /** 
+   * Wheter the icon is left or right of the input
+   */
+  side: 'left' | 'right';
+};
+
+/** Simple input with variable styling (depending on mode) */
+export const TextInput: React.FC<TextInputProps> = ({
+  mode = 'default',
+  side = 'right',
+  disabled,
+  isSelected,
+  adornment,
+  ...props
+}) => {
+  return (
+    <Container data-testid="input" {...{mode,disabled,isSelected,side}} >
+      <StyledInput {...props} disabled={disabled}/>
+      {adornment}
+    </Container>
+  );
+};
+
+type StyledCotainerProps = Pick<TextInputProps, 'mode' | 'disabled' | 'isSelected' | 'side'>;
+
+export const Container = styled.div.attrs(({mode, disabled, isSelected, side}: StyledCotainerProps) => {
+    let className = `${!disabled && 'bg-ui-0'} flex space-x-1.5 space-x-1.5
+    focus:outline-none focus-within:ring-2 focus-within:ring-primary-500 py-1.5 px-2
+    rounded-xl hover:border-ui-300 border-2 active:border-primary-500
+    ${side === 'left' && 'flex-row-reverse space-x-reverse'} items-center `;
+
+    if (isSelected) {
+      className += 'border-primary-500'
+    } else if (mode === 'default') {
+      className += 'border-ui-100';
+    } else if (mode === 'success') {
+      className += 'border-success-600';
+    } else if (mode === 'warning') {
+      className += 'border-warning-600';
+    } else if (mode === 'critical') {
+      className += 'border-critical-600';
+    }
+
+    return {className};
+})<StyledCotainerProps>``
+
+export const StyledInput = styled.input.attrs({
+  className:'w-full bg-transparent focus:outline-none'
+})``
