4bf194b25eb91cd2d4740110c1cfe7a56e7fa763
Fabrice Francois <Fabricevladimir@gmail.com>
2021-12-01 08:56:29 -0500

    hooks for data fetching

---
+++ packages/web-app/src/hooks/usePollTokens.tsx
@@ -0,0 +1,58 @@
+import {useCallback, useState} from 'react';
+
+import useInterval from 'hooks/useInterval';
+import useIsMounted from 'hooks/useIsMounted';
+import {fetchTokenUsdPrice} from 'services/prices';
+
+type TokenPrices = {
+  [key: string]: string | null;
+};
+
+/**
+ * Hook for fetching token prices at specified intervals
+ * @param tokenList List of token symbols or addresses to fetch USD value for
+ * @param interval Delay in milliseconds
+ * @returns Object with key value pairs corresponding to token address and USD value respectively,
+ * OR a string USD value if only one token address was provided.
+ * If USD value isn't found, returns null for token price.
+ *
+ * @example
+ * // multiple addresses
+ * const {prices, isLoading} = usePollTokens(tokenList, 1000);
+ * console.log(prices) // { 0x123...34fd: '5.0045', 0x123...fa23: null};
+ *
+ * // single address
+ * const {price, isLoading} = usePollTokens(ETH, 1000);
+ * console.log(price) // '4521.1420'
+ */
+const usePollTokens = (tokenList: string[], interval?: number) => {
+  const isMounted = useIsMounted();
+  const [prices, setPrices] = useState<TokenPrices>({});
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const fetchPrices = useCallback(
+    async (tokens: string[]) => {
+      setIsLoading(true);
+      for (let index = 0; index < tokens.length; index++) {
+        const address = tokens[index];
+        const price = await fetchTokenUsdPrice(address);
+
+        if (isMounted()) {
+          setPrices(prices => ({
+            ...prices,
+            [address]: price || null,
+          }));
+        }
+      }
+      setIsLoading(false);
+    },
+    [isMounted]
+  );
+
+  useInterval(() => fetchPrices(tokenList), interval);
+  return tokenList.length === 1
+    ? {price: prices[tokenList[0]], isLoading}
+    : {prices, isLoading};
+};
+
+export default usePollTokens;
