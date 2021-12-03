25250b463bf26cdab4dbb6c35fd8f540177229ec
Fabrice Francois <Fabricevladimir@gmail.com>
2021-12-03 13:51:00 -0500

    added transfer avatar

--- packages/ui-components/__tests__/transferListItem.test.tsx
+++ packages/ui-components/__tests__/transferListItem.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 
-import {Default as TransferListItem} from '../stories/transferListItem.stories';
+import {Deposit as TransferListItem} from '../stories/transferListItem.stories';
 
 describe('TransferListItem', () => {
   function setup(args?: any) {
--- packages/ui-components/src/components/transferListItem/transferListItem.tsx
+++ packages/ui-components/src/components/transferListItem/transferListItem.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {IconChevronRight} from '../icons';
+import {
+  IconDeposit,
+  IconSpinner,
+  IconWithdraw,
+  IconChevronRight,
+} from '../icons';
 
 export type TransferListItemProps = {
   isPending?: boolean;
@@ -18,37 +23,54 @@ export type TransferListItemProps = {
    * Date transfer was executed or a loading indication if transfer is still pending
    */
   transferDate: string;
-  transferType: 'Deposit' | 'Withdraw'; // TODO: Investigate proper smartcontract transfer type
+  transferType: 'Deposit' | 'Withdraw';
   usdValue: string;
   onClick?: () => void;
 };
 
-// TOOD: investigate transfer to determine when transfer is pending
-// (no date perhaps ? or custom check on resolved promise ?)
+const Icons: {[key: string]: JSX.Element} = {
+  Deposit: (
+    <IconDeposit className="h-1.5 w-1.5 desktop:h-2 desktop:w-2 text-success-600" />
+  ),
+  Pending: (
+    <IconSpinner className="h-1.5 w-1.5 desktop:h-2 desktop:w-2 text-primary-500" />
+  ),
+  Withdraw: (
+    <IconWithdraw className="h-1.5 w-1.5 desktop:h-2 desktop:w-2 text-warning-600" />
+  ),
+};
+
+const bgColors: {[key: string]: string} = {
+  Deposit: 'bg-success-100',
+  Pending: 'bg-primary-50',
+  Withdraw: 'bg-warning-100',
+};
+
 export const TransferListItem: React.FC<TransferListItemProps> = ({
   isPending = false,
   title,
   tokenAmount,
   tokenSymbol,
   transferDate,
+  transferType,
   usdValue,
   onClick,
 }) => {
   return (
     <Container data-testid="transferListItem" onClick={onClick}>
-      {/* TODO: Replace placeholder with appropriate icons */}
-
-      {isPending ? (
-        <div className="w-3 h-3 border desktop:w-5 desktop:h-5">...</div>
-      ) : (
-        <div className="w-3 h-3 border desktop:w-5 desktop:h-5" />
-      )}
+      <AvatarContainer
+        bgColor={isPending ? bgColors.Pending : bgColors[transferType]}
+      >
+        {isPending ? Icons.Pending : Icons[transferType]}
+      </AvatarContainer>
       <Content>
         <Title>{title}</Title>
         <Date>{transferDate}</Date>
       </Content>
       <Value>
-        <USDValue>{usdValue}</USDValue>
+        <USDValue>{`${
+          transferType === 'Deposit' ? '+' : '-'
+        } ${usdValue}`}</USDValue>
         <TokenAmount>{`${tokenAmount} ${tokenSymbol}`}</TokenAmount>
       </Value>
       <IconChevronRight className="text-ui-300 group-hover:text-primary-500" />
@@ -57,9 +79,13 @@ export const TransferListItem: React.FC<TransferListItemProps> = ({
 };
 
 const Container = styled.button.attrs({
-  className:
-    'group w-full px-2 desktop:px-3 py-1.5 desktop:py-2.5 bg-ui-0 rounded-xl flex items-center space-x-2 focus:outline-none focus:ring-2 focus:ring-primary-500 active:bg-ui-100',
+  className: `group w-full px-2 desktop:px-3 py-1.5 desktop:py-2.5 bg-ui-0 rounded-xl 
+  flex items-center space-x-2 focus:outline-none focus:ring-2 focus:ring-primary-500 active:bg-ui-100`,
 })``;
+
+const AvatarContainer = styled.div.attrs(({bgColor}: {bgColor: string}) => ({
+  className: `flex items-center justify-center w-3 h-3 ${bgColor} rounded desktop:w-5 desktop:h-5 desktop:rounded-xl`,
+}))<{bgColor: string}>``;
 
 const Content = styled.div.attrs({
   className: 'flex-1 text-left min-w-0',
