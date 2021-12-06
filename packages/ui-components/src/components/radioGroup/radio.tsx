5ccb3638ebe2284c11c77aa38aac50d85833631d
rakeshup <parthiban.rakesh95@gmail.com>
2021-12-06 16:57:59 +0530

    Radio group component, minor fixes for Token component

---
+++ packages/ui-components/src/components/radioGroup/radio.tsx
@@ -0,0 +1,26 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import styled from 'styled-components';
+import { useRadioGroupContext } from '.';
+
+type RadioProps = {
+  value: string;
+}
+
+export const Radio: React.FC<RadioProps> = ({ value, children }) => {
+  const { selectedValue, onChange } = useRadioGroupContext();
+
+  return (
+    <StyledRadioButton active={selectedValue === value} value={value} onClick={() => onChange(value)}>
+      {children}
+    </StyledRadioButton>
+  );
+};
+
+interface StyledRadioButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  active: boolean;
+}
+
+const StyledRadioButton = styled.button.attrs(({ active }: StyledRadioButtonProps) => ({
+  className: `p-1 font-bold ${active ? 'bg-ui-0 text-primary-500' : 'text-ui-500'}`,
+  style: { borderRadius: '0.625rem', minWidth: '4rem' },
+})) <StyledRadioButtonProps>``;
