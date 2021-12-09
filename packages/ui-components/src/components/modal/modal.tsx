ba8e5c4584851cea2b920af40ef6bd3d13e90bca
Sepehr sanaee <sepehr_sanaee78@yahoo.com>
2021-12-09 12:01:21 +0330

    modal added

---
+++ packages/ui-components/src/components/modal/modal.tsx
@@ -0,0 +1,97 @@
+import React, {HTMLAttributes, ReactNode, CSSProperties} from 'react';
+import styled from 'styled-components';
+import {Root, Title, Content, Close} from '@radix-ui/react-dialog';
+import {Backdrop} from '../backdrop';
+import {IconClose} from '../icons'
+
+export interface ModalProps extends HTMLAttributes<HTMLElement> {
+  /**
+   * The controlled open state of the popover. Must be used in conjunction with onOpenChange.
+   */
+  open?: boolean;
+  /**
+   * Event handler called when the open state of the popover changes.
+   */
+  onOpenChange?: (open: boolean) => void;
+  /**
+   * Modal title. if the title exists close button will appear
+   */
+  title: string;
+  /**
+   * Content
+   */
+  content: ReactNode;
+  /**
+  * The `onClose` prop allows passing a function that will be called once the modal has been dismissed.
+  */
+  onClose: () => void;
+}
+
+/**
+ * Default UI component
+ */
+export const Modal: React.FC<ModalProps> = ({
+  title,
+  content,
+  open = true,
+  onOpenChange,
+  onClose,
+  ...props
+}) => {
+  return (
+    <>
+      <Backdrop visible={open} onClose={onClose}/>
+      <Root {...{ open, onOpenChange }}>
+        <ModalContent
+          data-testid="modal-content"
+          {...props}
+          >
+          {title && <ModalHeader>
+            <ModalTitle>{title}</ModalTitle>
+            <ModalClose>
+              <IconClose height={14} width={14} />
+            </ModalClose>
+          </ModalHeader>}
+          {content}
+        </ModalContent>
+      </Root>
+    </>
+  );
+};
+
+
+type StyledContentProps = {
+  style?: CSSProperties | undefined;
+};
+
+const ModalContent = styled(Content).attrs(
+  ({style}: StyledContentProps) => {
+    const currentStyle: CSSProperties = style || {
+      background: '#FFFFFF',
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      boxShadow:'0px 24px 32px rgba(31, 41, 51, 0.04), 0px 16px 24px rgba(31, 41, 51, 0.04), 0px 4px 8px rgba(31, 41, 51, 0.04), 0px 0px 1px rgba(31, 41, 51, 0.04)',
+      borderRadius: 12,
+      width: '90vw',
+      maxWidth: '450px',
+      maxHeight: '85vh',
+      outline: 'none',
+    };
+
+    return {style: currentStyle};
+  }
+)<StyledContentProps>``;
+
+const ModalTitle = styled(Title).attrs({
+  className: 'ft-text-lg font-semibold py-2 px-3',
+})``;
+
+const ModalHeader = styled.div.attrs({
+  className:'flex justify-between',
+})``;
+
+const ModalClose = styled(Close).attrs({
+  className: 'text-ui-500',
+})``;
