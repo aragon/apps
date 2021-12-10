9e2f7dc4f1a14d54feac66ca72dcddc3e70d83b5
Fabrice <Fabricevladimir@gmail.com>
2021-12-10 02:52:52 -0500

    removed open button

--- packages/ui-components/src/components/button/index.tsx
+++ packages/ui-components/src/components/button/index.tsx
@@ -1,6 +1,5 @@
 export {ButtonProps, Button} from './button';
 export * from './buttonText';
-export * from './openButton';
 export {IconButton, IconButtonProps} from './iconButton';
 export {WalletButton, WalletButtonProps} from './walletButton';
 export * from './menuButton';
