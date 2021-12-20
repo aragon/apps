731c11d10e6649418f3e78afa72962a2f4888db1
Sepehr sanaee <sepehr_sanaee78@yahoo.com>
2021-12-20 11:21:01 +0330

    no token found message added

--- packages/web-app/src/containers/tokenMenu/index.tsx
+++ packages/web-app/src/containers/tokenMenu/index.tsx
@@ -2,6 +2,7 @@ import React, {useCallback} from 'react';
 import styled from 'styled-components';
 import {Modal, SearchInput, ButtonText, IconAdd} from '@aragon/ui-components';
 import {Wallet} from 'use-wallet/dist/cjs/types';
+import {useTranslation} from 'react-i18next';
 
 import {useWallet} from 'context/augmentedWallet';
 import {useTransferModalContext} from 'context/transfersModal';
@@ -12,7 +13,8 @@ import {useForm, Controller} from 'react-hook-form';
 const TokenMenu: React.FC = () => {
   const {isTokenOpen, close} = useTransferModalContext();
   const {chainId}: Wallet = useWallet();
-  const {control, watch, reset} = useForm();
+  const {control, watch} = useForm();
+  const {t} = useTranslation();
 
   const filterValidator = useCallback(
     (token: string[]) => {
@@ -26,13 +28,35 @@ const TokenMenu: React.FC = () => {
     [watch]
   );
 
-  const ResetSearchBox = () => {
-    reset({
-      searchToken: '',
-    });
+  const renderResult = () => {
+    const tokenList = Object.entries(
+      networks[chainId || 4].curatedTokens
+    ).filter(filterValidator);
+
+    return tokenList.length !== 0 ? (
+      <>
+        <TokenTitle>{t('TokenModal.yourToken')}</TokenTitle>
+        {tokenList.map(token => (
+          <TokenBox
+            tokenName={token[0] as string}
+            tokenAddress={token[1] as string}
+            tokenLogo={
+              'https://assets.coingecko.com/coins/images/681/small/JelZ58cv_400x400.png?1601449653'
+            }
+            key={token[1] as string}
+          />
+        ))}
+      </>
+    ) : (
+      <NoTokenWrapper>
+        <TokenTitle>{t('TokenModal.tokenNotFoundTitle')}</TokenTitle>
+        <TokenSubtitle>{t('TokenModal.tokenNotFoundSubtitle')}</TokenSubtitle>
+      </NoTokenWrapper>
+    );
   };
 
   //TODO: tokenLogo should be automate using coinkego api
+  //TODO: Cross Icon should added in the next released
   return (
     <Modal
       open={isTokenOpen}
@@ -45,30 +69,13 @@ const TokenMenu: React.FC = () => {
             <SearchInput
               {...{value, onChange}}
               placeholder="Type to filter ..."
-              rightAdornmentOnClick={ResetSearchBox}
             />
           )}
           name="searchToken"
           defaultValue={''}
           control={control}
         />
-        <TokensWrapper>
-          <TokensTitle>Your Tokens</TokensTitle>
-          {Object.entries(networks[chainId || 4].curatedTokens)
-            .filter(filterValidator)
-            .map(token => {
-              return (
-                <TokenBox
-                  tokenName={token[0] as string}
-                  tokenAddress={token[1] as string}
-                  tokenLogo={
-                    'https://assets.coingecko.com/coins/images/681/small/JelZ58cv_400x400.png?1601449653'
-                  }
-                  key={token[1] as string}
-                />
-              );
-            })}
-        </TokensWrapper>
+        <TokensWrapper>{renderResult()}</TokensWrapper>
         <WideButton
           mode="secondary"
           size="large"
@@ -90,10 +97,18 @@ const TokensWrapper = styled.div.attrs({
   className: 'space-y-1 mt-1',
 })``;
 
-const TokensTitle = styled.h2.attrs({
-  className: 'ft-text-lg font-bold',
+const TokenTitle = styled.h2.attrs({
+  className: 'text-base font-bold',
+})``;
+
+const TokenSubtitle = styled.h2.attrs({
+  className: 'text-sm',
 })``;
 
 const WideButton = styled(ButtonText).attrs({
   className: 'w-full justify-center',
+})``;
+
+const NoTokenWrapper = styled.div.attrs({
+  className: 'space-y-0.5',
 })``;
