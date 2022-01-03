46b340538b1b18787c3060509157001576e6bef4
RakeshUP <parthiban.rakesh95@gmail.com>
2022-01-03 15:16:48 +0530

    Review changes

--- packages/ui-components/src/components/cards/cardText.tsx
+++ packages/ui-components/src/components/cards/cardText.tsx
@@ -19,14 +19,14 @@ export const CardText: React.FC<CardTextProps> = ({type, title, content, bgWhite
 
 type ContainerProps = Pick<CardTextProps, 'bgWhite'>;
 
-const Container = styled.div.attrs(({bgWhite}:ContainerProps) => {
+const Container = styled.div.attrs(({bgWhite} : ContainerProps) => {
   const className = `${!bgWhite && 'bg-ui-0'} p-2 tablet:p-3 font-normal rounded-xl space-y-1 text-ui-600`;
   return {className};
 })<ContainerProps>``;
 
 type TitleProps = Pick<CardTextProps, 'type'>;
 
-const Title = styled.p.attrs(({type}:TitleProps) => {
+const Title = styled.p.attrs(({type} : TitleProps) => {
   const className = `${type === 'label' ? 'text-sm  text-ui-500': 'text-base text-ui-800'} font-bold`;
   return {className};
 })<TitleProps>``;
