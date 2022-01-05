6bbff227499d8fc1c1d6c0572f5d2ca0e396c19b
Fabrice <Fabricevladimir@gmail.com>
2022-01-05 11:01:21 -0500

    setup review deposit page

--- packages/web-app/src/containers/reviewDeposit/index.tsx
+++ packages/web-app/src/containers/reviewDeposit/index.tsx
@@ -3,22 +3,50 @@ import {useFormContext} from 'react-hook-form';
 import React, {useEffect, useState} from 'react';
 import {CardText, CardToken, CardTransfer} from '@aragon/ui-components';
 
-import {fetchTokenPrice} from 'services/prices';
+import {useWallet} from 'context/augmentedWallet';
+import {fetchTokenData, fetchTokenPrice} from 'services/prices';
 
 const ReviewDeposit: React.FC = () => {
   const {t} = useTranslation();
-  const {getValues} = useFormContext();
-  const [price, setPrice] = useState('$0.0');
+  const [price, setPrice] = useState<string>();
+  const {getValues, setValue} = useFormContext();
   const values = getValues();
+
+  const {provider} = useWallet();
 
   useEffect(() => {
     async function getPrice() {
+      // TODO: move metadata fetching to validate for contractAddress
+      if (values.isCustomToken) {
+        const data = await fetchTokenData(values.tokenAddress);
+        if (data) {
+          setValue('tokenName', data.name);
+          setValue('tokenSymbol', data.symbol);
+          setValue('tokenAddress', data.address);
+          setValue('tokenImgUrl', data.imgUrl);
+        }
+      }
+      // <----------------------------------------------------------------->
+
       const tokenPrice = await fetchTokenPrice(values.tokenAddress);
-      setPrice(tokenPrice);
+      if (tokenPrice) {
+        setPrice(
+          new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(tokenPrice * values.amount)
+        );
+      }
     }
 
     getPrice();
-  }, [values.tokenAddress]);
+  }, [
+    provider,
+    setValue,
+    values.amount,
+    values.isCustomToken,
+    values.tokenAddress,
+  ]);
 
   return (
     <div className="flex flex-col space-y-1.5 desktop:space-y-3">
