896090fcc8a38b190b3003f2340fe9061ee37d16
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-01-10 21:26:22 +0100

    add search functionality to modal

--- packages/web-app/src/containers/utcMenu/index.tsx
+++ packages/web-app/src/containers/utcMenu/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {useTranslation} from 'react-i18next';
-import {Modal, IconChevronRight, ListItemText} from '@aragon/ui-components';
+import {
+  Modal,
+  IconChevronRight,
+  ListItemText,
+  SearchInput,
+} from '@aragon/ui-components';
 
 import {useTransferModalContext} from 'context/transfersModal';
 import {timezones} from './utcData';
 
 const UtcMenu: React.FC = () => {
   const {isUtcOpen, close} = useTransferModalContext();
+  const [searchTerm, setSearchTerm] = useState<string>('');
   const {t} = useTranslation();
 
   const handleUtcClick = () => {
     close('utc');
   };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredTimezones = timezones.filter(tz => tz.includes(searchTerm));
 
   return (
     <Modal
@@ -21,26 +33,35 @@ const UtcMenu: React.FC = () => {
       title={t('newWithdraw.configureWithdraw.utcMenu.title') as string}
       data-testid="walletCard"
     >
-      <Container>
-        <ScrollableDiv>
-          {timezones.map((tz: string) => {
-            return (
-              <ListItemText
-                mode="default"
-                key={tz}
-                title={tz}
-                iconRight={<IconChevronRight />}
-                onClick={() => handleUtcClick()}
-              />
-            );
-          })}
-        </ScrollableDiv>
-      </Container>
+      <ModalBody>
+        <SearchInput
+          placeholder="Type to filter"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <Container>
+          <ScrollableDiv>
+            {filteredTimezones.map((tz: string) => {
+              return (
+                <ListItemText
+                  mode="default"
+                  key={tz}
+                  title={tz}
+                  iconRight={<IconChevronRight />}
+                  onClick={() => handleUtcClick()}
+                />
+              );
+            })}
+          </ScrollableDiv>
+        </Container>
+      </ModalBody>
     </Modal>
   );
 };
 
 export default UtcMenu;
+
+const ModalBody = styled.div.attrs({className: 'space-y-2'})``;
 
 const Container = styled.div.attrs({
   className: 'space-y-1 overflow-y-auto',
