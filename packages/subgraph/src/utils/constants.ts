56fcc39e272c13eda2e26268b9e52510caed6ff4
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-01-18 12:10:38 +0100

    fix typo

--- packages/subgraph/src/utils/constants.ts
+++ packages/subgraph/src/utils/constants.ts
@@ -1,4 +1,4 @@
-// AS do not support initializing Map eith data, we use a cain of sets instead
+// AS do not support initializing Map with data, we use a cain of sets instead
 export const EXECUTION_STATES = new Map<number, string>()
   .set(0, 'RUNNING')
   .set(1, 'STOPPED')
