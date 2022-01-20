e1373839176883087f5572afd1c5359a40d29f41
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-01-20 16:31:17 +0100

    deploy-init

--- packages/contracts/contracts/factories/DAOFactory.sol
+++ packages/contracts/contracts/factories/DAOFactory.sol
@@ -4,24 +4,24 @@
 
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts/proxy/Clones.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "./../processes/votings/simple/SimpleVoting.sol";
-import "./../tokens/GovernanceERC20.sol";
-import "./../tokens/GovernanceWrappedERC20.sol";
-import "./../core/processes/Process.sol";
-import "./../registry/Registry.sol";
-import "./../core/DAO.sol";
+import '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol';
+import '@openzeppelin/contracts/proxy/Clones.sol';
+import '@openzeppelin/contracts/utils/Address.sol';
+import './../processes/votings/simple/SimpleVoting.sol';
+import './../tokens/GovernanceERC20.sol';
+import './../tokens/GovernanceWrappedERC20.sol';
+import './../core/processes/Process.sol';
+import './../registry/Registry.sol';
+import './../core/DAO.sol';
 
-import "../utils/Proxy.sol";
+import '../utils/Proxy.sol';
 
 /// @title DAOFactory to create a DAO
 /// @author Giorgi Lagidze & Samuel Furter - Aragon Association - 2022
 /// @notice This contract is used to create a DAO.
 contract DAOFactory {
     using Address for address;
-    
+
     address private votingBase;
     address private daoBase;
     address private governanceERC20Base;
@@ -54,35 +54,40 @@ contract DAOFactory {
         bytes calldata _metadata,
         TokenConfig calldata _tokenConfig,
         uint256[3] calldata _votingSettings
-    ) external returns (DAO dao, SimpleVoting voting, address token) {
+    )
+        external
+        returns (
+            DAO dao,
+            SimpleVoting voting,
+            address token
+        )
+    {
         // setup Token
-        // TODO: Do we wanna leave the option not to use any proxy pattern in such case ? 
+        // TODO: Do we wanna leave the option not to use any proxy pattern in such case ?
         // delegateCall is costly if so many calls are needed for a contract after the deployment.
         token = _tokenConfig.addr;
         // https://forum.openzeppelin.com/t/what-is-the-best-practice-for-initializing-a-clone-created-with-openzeppelin-contracts-proxy-clones-sol/16681
-        if(token == address(0)) {
+        if (token == address(0)) {
             token = Clones.clone(governanceERC20Base);
-            GovernanceERC20(token).initialize(_tokenConfig.name, _tokenConfig.symbol);
+            GovernanceERC20(token).initialize(
+                _tokenConfig.name,
+                _tokenConfig.symbol
+            );
         } else {
             token = Clones.clone(governanceWrappedERC20Base);
             // user already has a token. we need to wrap it in our new token to make it governance token.
-            GovernanceWrappedERC20(
-                token
-            ).initialize(
+            GovernanceWrappedERC20(token).initialize(
                 IERC20Upgradeable(_tokenConfig.addr),
                 _tokenConfig.name,
                 _tokenConfig.symbol
             );
         }
 
-        dao = DAO(createProxy(daoBase, bytes("")));
-        
+        dao = DAO(createProxy(daoBase, bytes('')));
+
         registry.register(name, dao, msg.sender, token);
-        
-        dao.initialize(
-            _metadata,
-            address(this)
-        );  
+
+        dao.initialize(_metadata, address(this));
 
         bytes[] memory allowedActions;
         voting = SimpleVoting(
@@ -105,17 +110,45 @@ contract DAOFactory {
         dao.addProcess(voting);
 
         ACLData.BulkItem[] memory items = new ACLData.BulkItem[](7);
-        
+
         // Grant DAO all the permissions required
-        items[0] = ACLData.BulkItem(ACLData.BulkOp.Grant, dao.DAO_CONFIG_ROLE(), address(dao));
-        items[1] = ACLData.BulkItem(ACLData.BulkOp.Grant, dao.WITHDRAW_ROLE(), address(dao));
-        items[2] = ACLData.BulkItem(ACLData.BulkOp.Grant, dao.UPGRADE_ROLE(), address(dao));
-        items[3] = ACLData.BulkItem(ACLData.BulkOp.Grant, dao.ROOT_ROLE(), address(dao));
-        items[4] = ACLData.BulkItem(ACLData.BulkOp.Grant, dao.SET_SIGNATURE_VALIDATOR_ROLE(), address(dao));
+        items[0] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            dao.DAO_CONFIG_ROLE(),
+            address(dao)
+        );
+        items[1] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            dao.WITHDRAW_ROLE(),
+            address(dao)
+        );
+        items[2] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            dao.UPGRADE_ROLE(),
+            address(dao)
+        );
+        items[3] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            dao.ROOT_ROLE(),
+            address(dao)
+        );
+        items[4] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            dao.SET_SIGNATURE_VALIDATOR_ROLE(),
+            address(dao)
+        );
 
         // Revoke permissions from factory
-        items[5] = ACLData.BulkItem(ACLData.BulkOp.Revoke, dao.DAO_CONFIG_ROLE(), address(this));
-        items[6] = ACLData.BulkItem(ACLData.BulkOp.Revoke, dao.ROOT_ROLE(), address(this));
+        items[5] = ACLData.BulkItem(
+            ACLData.BulkOp.Revoke,
+            dao.DAO_CONFIG_ROLE(),
+            address(this)
+        );
+        items[6] = ACLData.BulkItem(
+            ACLData.BulkOp.Revoke,
+            dao.ROOT_ROLE(),
+            address(this)
+        );
 
         dao.bulk(address(dao), items);
 
@@ -123,15 +156,31 @@ contract DAOFactory {
         items = new ACLData.BulkItem[](4);
 
         address ANY_ADDR = address(type(uint160).max);
-        
-        items[0] = ACLData.BulkItem(ACLData.BulkOp.Grant, voting.PROCESS_VOTE_ROLE(), ANY_ADDR);
-        items[1] = ACLData.BulkItem(ACLData.BulkOp.Grant, voting.PROCESS_EXECUTE_ROLE(), ANY_ADDR);
-        items[2] = ACLData.BulkItem(ACLData.BulkOp.Grant, voting.PROCESS_START_ROLE(), ANY_ADDR);
-        items[3] = ACLData.BulkItem(ACLData.BulkOp.Grant, voting.MODIFY_CONFIG(), address(dao));
+
+        items[0] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            voting.PROCESS_VOTE_ROLE(),
+            ANY_ADDR
+        );
+        items[1] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            voting.PROCESS_EXECUTE_ROLE(),
+            ANY_ADDR
+        );
+        items[2] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            voting.PROCESS_START_ROLE(),
+            ANY_ADDR
+        );
+        items[3] = ACLData.BulkItem(
+            ACLData.BulkOp.Grant,
+            voting.MODIFY_CONFIG(),
+            address(dao)
+        );
 
         dao.bulk(address(voting), items);
     }
-    
+
     // @dev Internal helper method to set up the required base contracts on DAOFactory deployment.
     function setupBases() private {
         votingBase = address(new SimpleVoting());
