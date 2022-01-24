8fdefda40dc8a4335f164a4df12776a0143dd578
RakeshUP <parthiban.rakesh95@gmail.com>
2022-01-24 13:56:58 +0530

    Stepper component WIP

---
+++ packages/web-app/src/components/fullScreenStepper/fullScreenStepper.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FullScreenStepper = () => {
+  return <h1>StepForm</h1>;
+};
+
+export default FullScreenStepper;
---
+++ packages/web-app/src/components/fullScreenStepper/step.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Step = () => {
+  return <h1>Step</h1>;
+};
+
+export default Step;
