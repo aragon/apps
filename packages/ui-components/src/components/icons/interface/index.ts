c140c8063a3ee6b64a791be32afa06dea2510eb0
Sepehr sanaee <sepehr_sanaee78@yahoo.com>
2022-01-24 15:25:13 +0330

    add storage icon

--- packages/ui-components/src/components/icons/interface/index.ts
+++ packages/ui-components/src/components/icons/interface/index.ts
@@ -32,3 +32,4 @@ export {IconWarning} from './icon_warning';
 export {IconWithdraw} from './icon_withdraw';
 export {IconMenuVertical} from './icon_menu_vertical';
 export {IconExpand} from './icon_expand';
+export {IconStorage} from './icon_storage';
