2ad297e152bdc765a0198e0783aa926469d490de
RakeshUP <parthiban.rakesh95@gmail.com>
2022-01-28 15:51:08 +0530

    removed demo

--- packages/web-app/src/pages/home.tsx
+++ packages/web-app/src/pages/home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import {useTranslation} from 'react-i18next';
 import {withTransaction} from '@elastic/apm-rum-react';
-import CardWithImage from 'components/cardWithImage';
 
 const Home: React.FC = () => {
   const {t} = useTranslation();
@@ -15,34 +14,6 @@ const Home: React.FC = () => {
           <Title>{t('title.part1')}</Title>
           <Subtitle>{t('title.part2')}</Subtitle>
         </div>
-      </div>
-
-      {/* This whole thing is just for a demo, will be removed before merging */}
-      <div className="tablet:flex mx-3 tablet:mx-40 space-y-3 tablet:space-y-0 tablet:space-x-3">
-        <CardWithImage
-          imgSrc="https://source.unsplash.com/sqdY_rJg8wg"
-          caption="Step 1"
-          title="Select Blockchain"
-          subtitle="Decide which blockchain the DAO should be at home on."
-        />
-        <CardWithImage
-          imgSrc="https://source.unsplash.com/sqdY_rJg8wg"
-          caption="Step 2"
-          title="Select"
-          subtitle="Decide which blockchain the DAO should be at home on."
-        />
-        <CardWithImage
-          imgSrc="https://source.unsplash.com/sqdY_rJg8wg"
-          caption="Step 3"
-          title="Blockchain"
-          subtitle="Decide which blockchain the DAO should be at home on."
-        />
-        <CardWithImage
-          imgSrc="https://source.unsplash.com/sqdY_rJg8wg"
-          caption="Step 4"
-          title="Configure Governance"
-          subtitle="How are decisions made in the DAO? How long may a proposal be open for voting? Decide which blockchain the DAO should be at home on."
-        />
       </div>
     </>
   );
