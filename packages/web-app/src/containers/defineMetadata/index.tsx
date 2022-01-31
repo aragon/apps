f79d49188f4bb3b618447e86b72ba163e9caf056
Fabrice <Fabricevladimir@gmail.com>
2022-01-31 12:04:24 -0500

    added controller to fields

--- packages/web-app/src/containers/defineMetadata/index.tsx
+++ packages/web-app/src/containers/defineMetadata/index.tsx
@@ -7,9 +7,11 @@ import {
 import React from 'react';
 import styled from 'styled-components';
 import {useTranslation} from 'react-i18next';
+import {Controller, useFormContext} from 'react-hook-form';
 
 const DefineMetadata: React.FC = () => {
   const {t} = useTranslation();
+  const {control} = useFormContext();
 
   return (
     <>
@@ -17,55 +19,83 @@ const DefineMetadata: React.FC = () => {
       <FormItem>
         <Label
           label={t('labels.daoName')}
-          helpText={t('createDao.defineMetadata.nameSubtitle')}
+          helpText={t('createDAO.step2.nameSubtitle')}
         />
-        <>
-          <TextInput placeholder={t('placeHolders.daoName')} />
-          <AlertInline label="Wire up field error message" mode="critical" />
-        </>
+
+        <Controller
+          name="daoName"
+          control={control}
+          render={({field, fieldState: {error}}) => (
+            <>
+              <TextInput {...field} placeholder={t('placeHolders.daoName')} />
+              {error?.message && (
+                <AlertInline label={error.message} mode="critical" />
+              )}
+            </>
+          )}
+        />
       </FormItem>
 
       {/* Logo */}
       <FormItem>
         <Label
           label={t('labels.logo')}
-          helpText={t('createDao.defineMetadata.logoSubtitle')}
+          helpText={t('createDAO.step2.logoSubtitle')}
           isOptional
           badgeLabel={t('labels.optional')}
         />
-        <>
-          {/* TODO: replace with proper logo component */}
-          <div className="flex justify-center items-center w-8 h-8 bg-ui-0 rounded-xl border-2 border-dashed">
-            +
-          </div>
-          <AlertInline label="Wire up field error message" mode="critical" />
-        </>
+        <Controller
+          name="daoLogo"
+          control={control}
+          render={({fieldState: {error}}) => (
+            <>
+              {/* TODO: replace with proper logo component */}
+              <div className="flex justify-center items-center w-8 h-8 bg-ui-0 rounded-xl border-2 border-dashed">
+                +
+              </div>
+              {error?.message && (
+                <AlertInline label={error.message} mode="critical" />
+              )}
+            </>
+          )}
+        />
       </FormItem>
 
       {/* Summary */}
       <FormItem>
         <Label
           label={t('labels.description')}
-          helpText={t('createDao.defineMetadata.descriptionSubtitle')}
+          helpText={t('createDAO.step2.descriptionSubtitle')}
         />
-        <>
-          <TextareaSimple placeholder={t('placeHolders.daoDescription')} />
-          <AlertInline label="Wire up field error message" mode="critical" />
-        </>
+        <Controller
+          name="daoName"
+          control={control}
+          render={({field, fieldState: {error}}) => (
+            <>
+              <TextareaSimple
+                {...field}
+                placeholder={t('placeHolders.daoDescription')}
+              />
+              {error?.message && (
+                <AlertInline label={error.message} mode="critical" />
+              )}
+            </>
+          )}
+        />
       </FormItem>
 
       {/* Links */}
       <FormItem>
         <Label
           label={t('labels.links')}
-          helpText={t('createDao.defineMetadata.linksSubtitle')}
+          helpText={t('createDAO.step2.linksSubtitle')}
         />
         <>
           {/* TODO: replace with proper logo component */}
           <div className="h-25 bg-ui-0 rounded-xl">
             Placeholder links component
           </div>
-          <AlertInline label="Wire up field error message" mode="critical" />
+          {/* <AlertInline label="Wire up field error message" mode="critical" /> */}
         </>
       </FormItem>
     </>
