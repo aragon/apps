1ae2524ca3263673fa6a69ba231c3f1368b071a5
RakeshUP <parthiban.rakesh95@gmail.com>
2022-02-01 19:49:52 +0530

    single proposal proposal completed

--- packages/web-app/src/pages/proposals.tsx
+++ packages/web-app/src/pages/proposals.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {withTransaction} from '@elastic/apm-rum-react';
 import {useNavigate, useParams} from 'react-router-dom';
 
-import {useProposal} from '../hooks/useProposal';
 import * as paths from 'utils/paths';
 import {
   Badge,
@@ -17,6 +16,7 @@ import {useTranslation} from 'react-i18next';
 import {Link} from '@aragon/ui-components';
 import ResourceList from 'components/resourceList';
 
+// TODO: This is just some mock data. Remove this while integration
 const publishedDone: ProgressStatusProps = {
   label: 'Published',
   mode: 'done',
@@ -34,60 +34,80 @@ const stepDataRunning: ProgressStatusProps[] = [
 ];
 
 const Proposal: React.FC = () => {
+  const [expandedProposal, setExpandedProposal] = useState(false);
   const {t} = useTranslation();
   const navigate = useNavigate();
   const {id} = useParams();
   if (!id) {
     navigate(paths.NotFound);
   }
-  const definedId = id as string;
-  const {
-    data: proposalData,
-    isLoading,
-    error,
-  } = useProposal('0x0000000000', definedId);
 
   return (
     <Content>
       <Header>Proposal {id}</Header>
-      {/* <p>This page contains the overview for proposal {id}</p>
-      <br />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error: {error.message}</p>
-      ) : (
-        <p>{JSON.stringify(proposalData, null, 2)}</p>
-      )} */}
-      <div className="flex items-center mt-3 space-x-1.5">
-        <Badge label="Finance" />
-        <Badge label="Withdraw" />
-        <p className="pl-1.5 text-sm text-ui-500">
+      <BadgeContainer>
+        <div className="flex space-x-1.5">
+          <Badge label="Finance" />
+          <Badge label="Withdraw" />
+        </div>
+        <ProposerLink>
           {t('governance.proposals.publishedBy')}{' '}
           <Link
             external
             label={'you'}
             // label={shortenAddress(publisherAddress || '')}
             // href={`${explorers[chainId]}${publisherAddress}`}
-            className="text-sm"
           />
-        </p>
-      </div>
-      <div className="my-3">
-        {/* TODO: Render content using Tiptap https://tiptap.dev/guide/output#option-1-read-only-instance-of-tiptap */}
+        </ProposerLink>
+      </BadgeContainer>
+
+      <SummaryText>
         As most community members know, Aragon has strived to deploy its
         products to more cost-efficient blockchain networks to facilitate more
         adoption. Since Aragon is building products on Arbitrum which will use
         Aragon Court, it seems to be a natural chain of choice for L2
         deployment.
-      </div>
-      <ButtonText
-        label="Read Full Proposal"
-        mode="secondary"
-        iconRight={<IconChevronDown />}
-      />
-      <div className="flex mt-8 space-x-3">
-        <div className="space-y-3 w-3/5">
+      </SummaryText>
+
+      {!expandedProposal && (
+        <ButtonContainer>
+          <ButtonText
+            label="Read Full Proposal"
+            mode="secondary"
+            iconRight={<IconChevronDown />}
+            onClick={() => setExpandedProposal(true)}
+          />
+        </ButtonContainer>
+      )}
+
+      <ContentContainer expandedProposal={expandedProposal}>
+        <ProposalContainer>
+          {/* TODO: Render content using Tiptap https://tiptap.dev/guide/output#option-1-read-only-instance-of-tiptap */}
+          {expandedProposal && (
+            <>
+              <h2 className="text-xl">Second level title</h2>
+              <p className="mt-1.5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                laoreet urna. Mauris eu vestibulum urna, vel lacinia velit.
+                Quisque hendrerit mi libero, quis sollicitudin magna porta
+                commodo. Sed ornare lacus ut leo rutrum, eu hendrerit massa
+                gravida. Maecenas vehicula dolor sed ante sagittis egestas.{' '}
+                <br />
+                Duis et tortor id enim ullamcorper bibendum eget sed dolor.
+                <br />
+                Unordered list #1
+                <br />
+                Unordered list #2 and so on Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Sed nec laoreet urna. Mauris eu
+                vestibulum urna, vel lacinia velit. Quisque hendrerit mi libero,
+                quis sollicitudin magna porta commodo. Sed ornare lacus ut leo
+                rutrum, eu hendrerit massa gravida. Maecenas vehicula dolor sed
+                ante sagittis egestas. Duis et tortor id enim ullamcorper
+                bibendum eget sed dolor.
+              </p>
+            </>
+          )}
+
           <div className="p-4 h-96 bg-white rounded-xl">
             Voting table placeholder
           </div>
@@ -104,8 +124,9 @@ const Proposal: React.FC = () => {
             tokenCount="15,000,230.2323"
             treasuryShare="$1000.0"
           />
-        </div>
-        <div className="space-y-3 w-2/5">
+        </ProposalContainer>
+
+        <AdditionalInfoContainer>
           <ResourceList
             links={[
               {
@@ -119,13 +140,58 @@ const Proposal: React.FC = () => {
             ]}
           />
           <WidgetStatus steps={stepDataRunning} />
-        </div>
-      </div>
+        </AdditionalInfoContainer>
+      </ContentContainer>
     </Content>
   );
 };
 
 export default withTransaction('Proposal', 'component')(Proposal);
 
-const Content = styled.div.attrs({className: 'tablet:w-4/5 mx-auto mt-3'})``;
+const Content = styled.div.attrs({
+  className: 'tablet:w-4/5 mx-auto px-2 pt-3 pb-14',
+})``;
+
 const Header = styled.p.attrs({className: 'font-bold text-ui-800 text-3xl'})``;
+
+const BadgeContainer = styled.div.attrs({
+  className: 'tablet:flex items-baseline mt-3 tablet:space-x-3',
+})``;
+
+const ProposerLink = styled.p.attrs({
+  className: 'mt-1.5 tablet:mt-0 text-ui-500',
+})``;
+
+const SummaryText = styled.p.attrs({
+  className: 'text-lg text-ui-600 mt-3',
+})``;
+
+const ButtonContainer = styled.div.attrs({
+  className: 'mt-3',
+})`
+  button {
+    @media (max-width: 767px) {
+      width: 100%;
+    }
+  }
+`;
+
+const ProposalContainer = styled.div.attrs({
+  className: 'space-y-3 tablet:w-3/5',
+})``;
+
+const AdditionalInfoContainer = styled.div.attrs({
+  className: 'space-y-3 tablet:w-2/5',
+})``;
+
+type ContentContainerProps = {
+  expandedProposal: boolean;
+};
+
+const ContentContainer = styled.div.attrs(
+  ({expandedProposal}: ContentContainerProps) => ({
+    className: `${
+      expandedProposal ? 'tablet:mt-5' : 'tablet:mt-8'
+    } mt-3 tablet:flex tablet:space-x-3 space-y-3 tablet:space-y-0`,
+  })
+)<ContentContainerProps>``;
