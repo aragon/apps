c9ef7c7d775b1e0911cab61388194e60f244f45c
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-02-02 00:31:57 +0100

    add module for abis

--- packages/subgraph/scripts/build-manifest.sh
+++ packages/subgraph/scripts/build-manifest.sh
@@ -4,10 +4,20 @@ NETWORK=$1
 FILE=$NETWORK'.json'
 DATA=manifest/data/$FILE
 
+if [ -z "$2" ] 
+  then
+    echo "No Module argument supplied"
+    ZARAGOZA_CONTRACTS_MODULE=$(node -e 'console.log(require("path").dirname(require.resolve("@aragon/zaragoza-contracts/package.json")))')
+  else
+    echo "Module argument supplied"
+    ZARAGOZA_CONTRACTS_MODULE=$2
+fi
+
 echo 'Generating manifest from data file: '$DATA
 cat $DATA
 
 mustache \
   $DATA \
   manifest/subgraph.placeholder.yaml \
+  | sed -e "s#\$ZARAGOZA_CONTRACTS_MODULE#$ZARAGOZA_CONTRACTS_MODULE#g" \
   > subgraph.yaml
