00ac7ccbd4c869698682b872b821ec92280294d9
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-02-06 03:15:18 +0100

    run docker compose

--- packages/subgraph/scripts/test-subgraph.sh
+++ packages/subgraph/scripts/test-subgraph.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
 if [ ! -d "$PWD/build/" ]; then
   echo "The /build doesn’t exist. Did you run: yarn build ?"
   exit 1
 fi
 
+if [ -z $TEST_BINARY ]
+then
+  TEST_BINARY="binary-linux-20"
+fi
+
 rm -r tests/.bin
 
-curl -OL https://github.com/LimeChain/matchstick/releases/download/0.2.3/binary-linux-20
-chmod a+x binary-linux-20
+curl -OL https://github.com/LimeChain/matchstick/releases/download/0.2.3/$TEST_BINARY
+chmod a+x $TEST_BINARY
 
-./binary-linux-20
+./$TEST_BINARY
 
-rm binary-linux-20
+rm $TEST_BINARY
