271017dfec01d360567dd0fe34672d6d73607fcb
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-02-07 15:03:14 +0100

    introduce date section

--- packages/web-app/src/containers/setupVotingForm/index.tsx
+++ packages/web-app/src/containers/setupVotingForm/index.tsx
@@ -298,12 +298,140 @@ const SetupVotingForm: React.FC = () => {
 
 export default SetupVotingForm;
 
+type DateSectionProps = {
+  isStartDate: boolean;
+};
+
+const DateSection: React.FC<DateSectionProps> = ({isStartDate}) => {
+  const {t} = useTranslation();
+  const {open} = useTransferModalContext();
+  const {control} = useFormContext();
+  const {errors} = useFormState({control});
+  const [startDate, endDate, duration] = useWatch({
+    name: ['startDate', 'endDate', 'duration'],
+  });
+
+  const [utc, setUtc] = useState('');
+
+  useEffect(() => {
+    const currTimezone = timezones.find(tz => tz === getCanonicalUtcOffset());
+    if (!currTimezone) {
+      setUtc(timezones[13]);
+    } else {
+      setUtc(currTimezone);
+    }
+  }, []);
+
+  const startDateValidator = (date: string) => {
+    const startDate = new Date(date);
+    const todayMidnight = new Date().setHours(0, 0, 0, 0);
+    // TODO remove
+    const currDay = new Date(todayMidnight);
+
+    if (startDate.getTime() < currDay.getTime()) {
+      return t('errors.startDatePast');
+    }
+    return '';
+  };
+
+  const startTimeValidator = (time: string) => {
+    const startDateTime = new Date(startDate + ' ' + time);
+    const currDate = new Date();
+
+    if (startDateTime.getTime() < currDate.getTime()) {
+      return t('errors.startTimePast');
+    }
+    return '';
+  };
+
+  const endDateValidator = (date: string) => {
+    const endDate = new Date(date);
+    const todayMidnight = new Date().setHours(0, 0, 0, 0);
+    const daysOffset = daysToMils(5);
+    const minMidnight = todayMidnight + daysOffset;
+
+    if (endDate.getTime() < minMidnight) {
+      return t('errors.endDatePast');
+    }
+    return '';
+  };
+
+  const endTimeValidator = (time: string) => {
+    const endDateTime = new Date(endDate + ' ' + time);
+    const daysOffset = daysToMils(5);
+    const minEndDateTime = new Date().getTime() + daysOffset;
+
+    if (endDateTime.getTime() < minEndDateTime) {
+      return t('errors.endDatePast');
+    }
+    return '';
+  };
+
+  const dateInputName = isStartDate ? 'startDate' : 'endDate';
+  const timeInputName = isStartDate ? 'startTime' : 'endTime';
+  const dateInputValidator = isStartDate
+    ? startDateValidator
+    : endDateValidator;
+  const timeInputValidator = isStartDate
+    ? startTimeValidator
+    : endTimeValidator;
+
+  return (
+    <HStack>
+      <Controller
+        name={dateInputName}
+        control={control}
+        rules={{
+          required: t('errors.required.time'),
+          validate: dateInputValidator,
+        }}
+        render={({field: {name, value, onChange}, fieldState: {error}}) => (
+          <div>
+            <DateInput name={name} value={value} onChange={onChange} />
+            {error?.message && (
+              <AlertWrapper>
+                <AlertInline label={error.message} mode="critical" />
+              </AlertWrapper>
+            )}
+          </div>
+        )}
+      />
+      <Controller
+        name={timeInputName}
+        control={control}
+        rules={{
+          required: t('errors.required.date'),
+          validate: timeInputValidator,
+        }}
+        render={({field: {name, value, onChange}, fieldState: {error}}) => (
+          <div>
+            <SimplifiedTimeInput
+              name={name}
+              value={value}
+              onChange={onChange}
+            />
+            {error?.message && (
+              <AlertWrapper>
+                <AlertInline label={error.message} mode="critical" />
+              </AlertWrapper>
+            )}
+          </div>
+        )}
+      />
+      <div>
+        <DropdownInput value={utc} onClick={() => open('utc')} />
+      </div>
+      <UtcMenu onTimezoneSelect={setUtc} />
+    </HStack>
+  );
+};
+
 type NetworkSwitchProps = {
   value: EndDateType;
   setValue: (value: EndDateType) => void;
 };
 
-const NetworkSwitch = ({value, setValue}: NetworkSwitchProps) => {
+const NetworkSwitch: React.FC<NetworkSwitchProps> = ({value, setValue}) => {
   const {t} = useTranslation();
 
   return (
