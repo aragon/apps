c7a229907eec110aa7b00270fe5363ff6f495724
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-02-08 21:45:02 +0100

    clean up

--- packages/subgraph/tests/utils.ts
+++ packages/subgraph/tests/utils.ts
@@ -1,5 +1,5 @@
 import {Address, ethereum} from '@graphprotocol/graph-ts';
-import {newMockEvent, createMockedFunction} from 'matchstick-as/assembly/index';
+import {createMockedFunction} from 'matchstick-as/assembly/index';
 
 export function createMockGetter(
   contractAddress: string,
