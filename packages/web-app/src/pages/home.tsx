a207625d03d3b2d00fa992a9b33639ba109a80d3
RakeshUP <parthiban.rakesh95@gmail.com>
2022-02-09 16:58:28 +0530

    voting terminal placeholder swapped

--- packages/web-app/src/pages/home.tsx
+++ packages/web-app/src/pages/home.tsx
@@ -4,11 +4,7 @@ import {useTranslation} from 'react-i18next';
 import {withTransaction} from '@elastic/apm-rum-react';
 import {ButtonText} from '@aragon/ui-components';
 import {useNavigate} from 'react-router-dom';
-<<<<<<< HEAD
-import {VotingTerminal} from 'containers/votingTerminal';
-=======
 import {useGlobalModalContext} from 'context/globalModals';
->>>>>>> b272bd4 (modal refactoring completed)
 
 const Home: React.FC = () => {
   const {t} = useTranslation();
@@ -24,18 +20,7 @@ const Home: React.FC = () => {
           <Subtitle>{t('title.part2')}</Subtitle>
         </div>
       </div>
-      <div className="flex justify-center">
-        <ButtonText
-          label="Create DAO"
-          size="large"
-          onClick={() => navigate('/create-dao')}
-        />
-      </div>
 
-      {/* This is just a demo, will be removed before merging */}
-      <div className="my-4 mx-auto w-11/12 tablet:w-1/2">
-        <VotingTerminal />
-      </div>
       <ButtonText
         label="Create DAO"
         className="mx-auto"
