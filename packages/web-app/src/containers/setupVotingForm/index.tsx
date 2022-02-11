c342b447eeab3c1134c691f492cb4ab0b61b09f2
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-02-11 10:21:31 +0100

    improve form error handling

--- packages/web-app/src/containers/setupVotingForm/index.tsx
+++ packages/web-app/src/containers/setupVotingForm/index.tsx
@@ -29,7 +29,8 @@ type UtcInstance = 'first' | 'second';
 const SetupVotingForm: React.FC = () => {
   const {t} = useTranslation();
   const {open} = useTransferModalContext();
-  const {control, setValue, getValues, setError, formState} = useFormContext();
+  const {control, setValue, getValues, setError, formState, clearErrors} =
+    useFormContext();
 
   /*************************************************
    *                    STATE & EFFECT             *
@@ -54,8 +55,6 @@ const SetupVotingForm: React.FC = () => {
       setValue('endUtc', currTimezone);
     }
   }, []);
-
-  console.log(JSON.stringify(formState.errors, null, 2));
 
   useEffect(() => {
     const fieldErrors: FieldError[] = Object.values(formState.errors);
@@ -99,21 +98,30 @@ const SetupVotingForm: React.FC = () => {
    *                Field Validators               *
    *************************************************/
 
-  const startDateTimeValidator = (input: string) => {
+  function buildStartDateTime(): number {
     const sDate = getValues('startDate');
     const sTime = getValues('startTime');
     const sUtc = getValues('startUtc');
+
     const canonicalSUtc = getCanonicalUtcOffset(sUtc);
     const startDateTime = toDate(sDate + 'T' + sTime + canonicalSUtc);
-    const startMills = startDateTime.valueOf();
+    return startDateTime.valueOf();
+  }
+
+  const startDateTimeValidator = (input: string) => {
+    console.log("TRACING location: 'startDateTimeValidator'");
+    const startMills = buildStartDateTime();
 
     const currDateTime = new Date();
     const currMills = currDateTime.getTime();
 
     if (startMills < currMills) {
       return t('errors.startPast');
+    } else {
+      clearErrors('startDate');
+      clearErrors('startTime');
+      return '';
     }
-    return '';
   };
 
   const endDateTimeValidator = (input: string) => {
@@ -122,29 +130,19 @@ const SetupVotingForm: React.FC = () => {
     const eUtc = getValues('endUtc');
     const canonicalEUtc = getCanonicalUtcOffset(eUtc);
     const endDateTime = toDate(eDate + 'T' + eTime + canonicalEUtc);
-    // console.log(endDateTime);
     const endMills = endDateTime.valueOf();
 
-    const sDate = getValues('startDate');
-    const sTime = getValues('startTime');
-    const sUtc = getValues('startUtc');
-    const canonicalSUtc = getCanonicalUtcOffset(sUtc);
-    const startDateTime = toDate(sDate + 'T' + sTime + canonicalSUtc);
-    // console.log(startDateTime);
-    const startMills = startDateTime.valueOf();
+    const startMills = buildStartDateTime();
     const minEndDateTimeMills = startMills + daysToMills(5);
 
     if (endMills < minEndDateTimeMills) {
+      console.log("TRACING location: 'endDateTimeValidator' error");
       return t('errors.endPast');
+    } else {
+      clearErrors('endDate');
+      clearErrors('endTime');
+      return '';
     }
-    return '';
-  };
-
-  const durationValidator = (duration: number) => {
-    if (duration < 5) {
-      return t('errors.durationTooShort');
-    }
-    return '';
   };
 
   const tzSelector = (tz: string) => {
@@ -208,7 +206,10 @@ const SetupVotingForm: React.FC = () => {
             control={control}
             rules={{
               required: t('errors.required.time'),
-              validate: startDateTimeValidator,
+              validate: {
+                endTime: endDateTimeValidator,
+                startTime: startDateTimeValidator,
+              },
             }}
             render={({field: {name, value, onChange, onBlur}}) => (
               <div>
@@ -241,100 +242,106 @@ const SetupVotingForm: React.FC = () => {
           helpText={t('newWithdraw.setupVoting.endDescription')}
         />
         {endDateType === 'duration' ? (
-          <HStack>
-            <DateModeSwitch value={endDateType} setValue={setEndDateType} />
-            <Controller
-              name="duration"
-              control={control}
-              rules={{
-                required: t('errors.required.duration'),
-                validate: durationValidator,
-              }}
-              render={({
-                field: {name, onChange, value},
-                fieldState: {error},
-              }) => (
-                <div>
-                  {/* FIXME: this is currently not working properly, because
+          <>
+            <HStack>
+              <DateModeSwitch value={endDateType} setValue={setEndDateType} />
+              <Controller
+                name="duration"
+                control={control}
+                rules={{
+                  min: {
+                    value: 5,
+                    message: t('errors.durationTooShort'),
+                  },
+                  required: t('errors.required.duration'),
+                }}
+                render={({field: {name, onChange, value}}) => {
+                  {
+                    /* FIXME: this is currently not working properly, because
                   we're mixing controlled and uncontrolled components. The
                   buttons inside the numeric input are handled with a ref, which
                   does not trigger onchange events. This means that we can
                   detect changes in the input when typing them, but not when
                   using the buttons. This is an issue when typing 0. This
                   correctly triggers the error state, but then does not trigger
-                  it again when using the buttons afterwards. */}
-                  <NumberInput
-                    mode={error?.message ? 'critical' : 'default'}
-                    name={name}
-                    value={value}
-                    min={5}
-                    onChange={onChange}
-                    width={144}
-                  />
-                  {error?.message && (
-                    <AlertWrapper>
-                      <AlertInline label={error.message} mode="critical" />
-                    </AlertWrapper>
-                  )}
-                </div>
-              )}
-            />
-          </HStack>
-        ) : (
-          <div className="block space-y-2">
-            <div>
-              <DateModeSwitch value={endDateType} setValue={setEndDateType} />
-            </div>
-            <HStack>
-              <Controller
-                name="endDate"
-                control={control}
-                rules={{
-                  required: t('errors.required.date'),
-                  validate: endDateTimeValidator,
-                }}
-                render={({field: {name, value, onChange, onBlur}}) => (
-                  <div>
-                    <DateInput
+                  it again when using the buttons afterwards. */
+                  }
+                  return (
+                    <NumberInput
                       name={name}
                       value={value}
+                      min={5}
                       onChange={onChange}
-                      onBlur={onBlur}
+                      width={144}
                     />
-                  </div>
-                )}
-              />
-              <Controller
-                name="endTime"
-                control={control}
-                rules={{
-                  required: t('errors.required.time'),
-                  validate: endDateTimeValidator,
+                  );
                 }}
-                render={({field: {name, value, onChange, onBlur}}) => (
-                  <div>
-                    <SimplifiedTimeInput
-                      name={name}
-                      value={value}
-                      onChange={onChange}
-                      onBlur={onBlur}
-                    />
-                  </div>
-                )}
               />
-              <div>
-                <DropdownInput
-                  value={utcEnd}
-                  onClick={() => {
-                    setUtcInstance('second');
-                    open('utc');
-                  }}
-                />
-              </div>
             </HStack>
-          </div>
+            {formState.errors?.duration?.message && (
+              <AlertInline
+                label={formState.errors.duration.message}
+                mode="critical"
+              />
+            )}
+          </>
+        ) : (
+          <>
+            <div className="block space-y-2">
+              <div>
+                <DateModeSwitch value={endDateType} setValue={setEndDateType} />
+              </div>
+              <HStack>
+                <Controller
+                  name="endDate"
+                  control={control}
+                  rules={{
+                    required: t('errors.required.date'),
+                    validate: endDateTimeValidator,
+                  }}
+                  render={({field: {name, value, onChange, onBlur}}) => (
+                    <div>
+                      <DateInput
+                        name={name}
+                        value={value}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                      />
+                    </div>
+                  )}
+                />
+                <Controller
+                  name="endTime"
+                  control={control}
+                  rules={{
+                    required: t('errors.required.time'),
+                    validate: endDateTimeValidator,
+                  }}
+                  render={({field: {name, value, onChange, onBlur}}) => (
+                    <div>
+                      <SimplifiedTimeInput
+                        name={name}
+                        value={value}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                      />
+                    </div>
+                  )}
+                />
+                <div>
+                  <DropdownInput
+                    value={utcEnd}
+                    onClick={() => {
+                      setUtcInstance('second');
+                      open('utc');
+                    }}
+                  />
+                </div>
+              </HStack>
+            </div>
+            <DateTimeErrors mode={'end'} />
+          </>
         )}
-        <DateTimeErrors mode={'end'} />
         <AlertInline label={t('infos.voteDuration')} mode="neutral" />
       </FormSection>
       <UtcMenu onTimezoneSelect={tzSelector} />
@@ -350,8 +357,4 @@ const FormSection = styled.div.attrs({
 
 const HStack = styled.div.attrs({
   className: 'inline-flex space-x-1',
-})``;
-
-const AlertWrapper = styled.div.attrs({
-  className: 'mt-1',
 })``;
