1ecc0a09d5f258de0efcbd9aa01d05c895c24b62
Fabrice <Fabricevladimir@gmail.com>
2022-04-13 15:01:44 -0400

    added typings for walletconnect

--- packages/web-app/vite.config.ts
+++ packages/web-app/vite.config.ts
@@ -2,10 +2,6 @@ import typescript from '@rollup/plugin-typescript';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import {defineConfig, loadEnv} from 'vite';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
-import inject from '@rollup/plugin-inject';
-
-const production = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -26,31 +22,10 @@ export default defineConfig(({mode}) => {
   return {
     base: '',
     plugins: [
-      // ↓ Needed for development mode
-      !production &&
-        nodePolyfills({
-          include: [
-            'node_modules/**/*.js',
-            new RegExp('node_modules/.vite/.*js'),
-          ],
-        }),
       htmlEnvPlugin(),
       reactRefresh(),
       tsconfigPaths(),
       typescript({tsconfig: './tsconfig.json'}),
     ],
-    optimizeDeps: {
-      exclude: ['web3'], // <= The libraries that need shimming should be excluded from dependency optimization.
-    },
-    build: {
-      sourcemap: true,
-      commonjsOptions: {
-        transformMixedEsModules: true,
-      },
-      rollupOptions: {
-        plugins: [inject({Buffer: ['Buffer', 'Buffer']})],
-      },
-      // ↓ Needed for build if using WalletConnect and other providers
-    },
   };
 });
