d78acc82a2fb1bbb55eab9362d828bb8b9904265
sepehr sanaee <sepehr2mail@gmail.com>
2022-04-28 14:54:21 +0430

    hero gradients added

--- packages/web-app/src/containers/hero/index.tsx
+++ packages/web-app/src/containers/hero/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from 'public/coloredLogo.svg';
+import Green from 'public/circleGreenGradient.svg';
+import Purple from 'public/purpleGradient.svg';
 
 function Hero() {
   return (
@@ -15,13 +17,17 @@ function Hero() {
       <div className="h-full">
         <StyledImage src={Logo} />
       </div>
+      <div className="absolute top-64 desktop:top-20 right-0 desktop:-right-2.5">
+        <GradientLeft src={Green} />
+        <GradientRight src={Purple} />
+      </div>
     </Container>
   );
 }
 
 const Container = styled.div.attrs({
   className:
-    'flex justify-between bg-primary-400 h-55 overflow-hidden desktop:px-10 px-2',
+    'flex justify-between bg-primary-400 h-55 overflow-hidden desktop:px-10 px-2 relative',
 })``;
 
 const ContentWrapper = styled.div.attrs({
@@ -40,6 +46,14 @@ const Subtitle = styled.h3.attrs({
 
 const StyledImage = styled.img.attrs({
   className: 'w-71 hidden desktop:block',
+})``;
+
+const GradientLeft = styled.img.attrs({
+  className: 'h-40 desktop:-mt-12 -mt-36 desktop:-ml-63 -ml-44',
+})``;
+
+const GradientRight = styled.img.attrs({
+  className: 'desktop:h-40 h-30 desktop:-mt-12 -mt-36 desktop:-mr-7',
 })``;
 
 export default Hero;
