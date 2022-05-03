d6d15e9cddaddc7a73fcb45c9dee57129572e273
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-05-03 17:12:46 +0200

    add dao exist query and hook

---
+++ packages/web-app/src/hooks/data/useDaoExists.tsx
@@ -0,0 +1,19 @@
+import {Address} from '@aragon/ui-components/dist/utils/addresses';
+import {useQuery} from '@apollo/client';
+
+import {DAO_BY_ADDRESS} from 'queries/dao';
+
+export const useDaoExists = (daoAddress: Address) => {
+  const {data, error, loading, refetch} = useQuery(DAO_BY_ADDRESS, {
+    variables: {id: daoAddress},
+  });
+
+  const daoExists: boolean = data?.dao?.id ? true : false;
+
+  return {
+    data: daoExists,
+    error,
+    loading,
+    refetch,
+  };
+};
