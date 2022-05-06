948ebac40fd8fe339fedbc383fec661a14df2135
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-05-06 12:29:22 +0200

    update community page

--- packages/web-app/src/pages/community.tsx
+++ packages/web-app/src/pages/community.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import {withTransaction} from '@elastic/apm-rum-react';
+import {PageWrapper} from 'components/wrappers';
 
 const Community: React.FC = () => {
-  return <h1>Community Page</h1>;
+  return (
+    <PageWrapper
+      title="Community Page"
+      subtitle="Placeholder for the Community page"
+      showButton={false}
+      buttonLabel={'sdf'}
+    >
+      <></>
+    </PageWrapper>
+  );
 };
 
 export default withTransaction('Community', 'component')(Community);
