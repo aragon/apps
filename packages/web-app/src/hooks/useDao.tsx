81dae62036a574d58f34ca0b4f080155aa1794f1
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-05-11 17:58:03 +0200

    add hook documentation

--- packages/web-app/src/hooks/useDao.tsx
+++ packages/web-app/src/hooks/useDao.tsx
@@ -4,6 +4,14 @@ import {useEffect} from 'react';
 import {useNavigate, useParams} from 'react-router-dom';
 import {NotFound} from 'utils/paths';
 
+/**
+ * This hook queries the current URL for a the DAO address and validates that a
+ * DAO with this address exists. This is done querying subgraph for a dao with
+ * the id parsed from the URL. If the dao exists, the id is simply returned. If
+ * it does not exist, the user is redirected to the 404 page.
+ *
+ * @returns A apollo query result containing the DAO id.
+ */
 export function useDaoParam() {
   const {dao} = useParams();
   // NOTE At this point, daoParam will always be defined.
