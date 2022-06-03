14a7b27eaf1da759bcf6a090e56f58e26f8f8648
Fabrice <Fabricevladimir@gmail.com>
2022-06-03 07:56:34 -0400

    added active proposal in snapshot

--- packages/web-app/src/hooks/useDaoProposals.tsx
+++ packages/web-app/src/hooks/useDaoProposals.tsx
@@ -30,8 +30,8 @@ const proposals = [
     ],
   },
   {
-    process: 'succeeded',
-    title: 'Aragon Court deployment on Arbitrum',
+    process: 'active',
+    title: 'New Founding for Tree Lovers SubDao',
     description:
       'As most community members know, Aragon has strived to deploy its products to more cost-efficient blockchain networks to facilitate ...',
     voteTitle: 'Winning Option',
@@ -41,29 +41,7 @@ const proposals = [
     tokenSymbol: 'DNT',
     publishLabel: 'Published by',
     publisherAddress: '0x374d444487A4602750CA00EFdaC5d22B21F130E1',
-    alertMessage: 'Starts in 3 days 5 hours',
-    stateLabel: [
-      'Draft',
-      'Pending',
-      'Active',
-      'Executed',
-      'Succeeded',
-      'Defeated',
-    ],
-  },
-  {
-    process: 'defeated',
-    title: 'New Founding for Lorex Lab SubDao',
-    description:
-      'As most community members know, Aragon has strived to deploy its products to more cost-efficient blockchain networks to facilitate ...',
-    voteTitle: 'Winning Option',
-    voteProgress: 70,
-    voteLabel: 'Yes',
-    tokenAmount: '3.5M',
-    tokenSymbol: 'DNT',
-    publishLabel: 'Published by',
-    publisherAddress: '0x374d444487A4602750CA00EFdaC5d22B21F130E1',
-    alertMessage: 'Starts in x days y hours',
+    alertMessage: '10 days 6 hours left',
     stateLabel: [
       'Draft',
       'Pending',
@@ -75,7 +53,7 @@ const proposals = [
   },
   {
     process: 'executed',
-    title: 'New Founding for Tree Lovers SubDao',
+    title: 'New Founding for Cat Hugging SubDao',
     description:
       'As most community members know, Aragon has strived to deploy its products to more cost-efficient blockchain networks to facilitate ...',
     voteTitle: 'Winning Option',
