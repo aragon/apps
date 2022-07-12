bb577ea6680e50acabe2dd40696e3ee74a7af33a
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-07-12 17:07:43 +0200

    remove metadata dependency

--- packages/web-app/src/components/proposalList/index.tsx
+++ packages/web-app/src/components/proposalList/index.tsx
@@ -27,45 +27,36 @@ const ProposalList: React.FC<ProposalListProps> = ({proposals}) => {
           proposal.startDate,
           proposal.endDate
         );
-        try {
-          const metadata = JSON.parse(proposal.metadata);
-
-          return (
-            <CardProposal
-              title={metadata.title}
-              description={metadata.summary}
-              onClick={() => {
-                navigate('proposals/' + proposal.id);
-              }}
-              process={proposal.type}
-              chainId={chainId}
-              voteTitle={t('governance.proposals.voteTitle') as string}
-              // Rakesh: Vote results seem to be aggregated in the subgraph. So after confirming that the
-              // below logic might change and also the function commented at the end of this file
-
-              // {...(categorizedProposal.type === 'active' && {
-              // voteProgress: getVoteResults(proposal.vote).toString(),
-              // voteLabel: proposal.yea.toString(),
-              // tokenAmount: proposal.total.toString(),
-              // tokenSymbol: proposal.vote.tokenSymbol,
-              // })}
-              publishLabel={t('governance.proposals.publishedBy') as string}
-              publisherAddress={proposal.creator}
-              stateLabel={[
-                t('governance.proposals.states.draft'),
-                t('governance.proposals.states.pending'),
-                t('governance.proposals.states.active'),
-                t('governance.proposals.states.executed'),
-                t('governance.proposals.states.succeeded'),
-                t('governance.proposals.states.defeated'),
-              ]}
-              {...(AlertMessage && {AlertMessage})}
-              key={proposal.id}
-            />
-          );
-        } catch {
-          return null;
-        }
+        return (
+          <CardProposal
+            title={'Title eventually coming from Metadata'}
+            description={'Summary eventually coming from Metadata'}
+            onClick={() => {
+              navigate('proposals/' + proposal.id);
+            }}
+            process={proposal.type}
+            chainId={chainId}
+            voteTitle={t('governance.proposals.voteTitle') as string}
+            // {...(proposal.type === 'active' && {
+            //   voteProgress: getVoteResults(proposal.vote).toString(),
+            //   voteLabel: proposal.yea.toString(),
+            //   tokenAmount: proposal.total.toString(),
+            //   tokenSymbol: proposal.vote.tokenSymbol,
+            // })}
+            publishLabel={t('governance.proposals.publishedBy') as string}
+            publisherAddress={proposal.creator}
+            stateLabel={[
+              t('governance.proposals.states.draft'),
+              t('governance.proposals.states.pending'),
+              t('governance.proposals.states.active'),
+              t('governance.proposals.states.executed'),
+              t('governance.proposals.states.succeeded'),
+              t('governance.proposals.states.defeated'),
+            ]}
+            {...(AlertMessage && {AlertMessage})}
+            key={proposal.id}
+          />
+        );
       })}
     </div>
   );
