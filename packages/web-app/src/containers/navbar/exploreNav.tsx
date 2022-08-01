a377713529bdee3285c615d1027b21f06cf00ca6
RakeshUP <parthiban.rakesh95@gmail.com>
2022-08-01 15:06:03 +0530

    multiple fixes

--- packages/web-app/src/containers/navbar/exploreNav.tsx
+++ packages/web-app/src/containers/navbar/exploreNav.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import {ButtonWallet, SearchInput, IconSearch} from '@aragon/ui-components';
+import {ButtonWallet} from '@aragon/ui-components';
+// import {ButtonWallet, SearchInput, IconSearch} from '@aragon/ui-components';
 import {useTranslation} from 'react-i18next';
 
 import {useWallet} from 'hooks/useWallet';
 import Logo from 'public/logo.svg';
-import useScreen from 'hooks/useScreen';
+// import useScreen from 'hooks/useScreen';
 import {useGlobalModalContext} from 'context/globalModals';
 import {Container, GridLayout} from 'components/layout';
 
 const ExploreNav: React.FC = () => {
   const {t} = useTranslation();
   const {address, ensName, ensAvatarUrl, isConnected, methods} = useWallet();
-  const {isDesktop} = useScreen();
+  // const {isDesktop} = useScreen();
   const {open} = useGlobalModalContext();
 
   const handleWalletButtonClick = () => {
@@ -36,14 +37,14 @@ const ExploreNav: React.FC = () => {
           </LeftContent>
           <RightContent>
             <ActionsWrapper>
-              {isDesktop ? (
+              {/* {isDesktop ? (
                 <SearchInput
                   placeholder={t('placeHolders.searchTokens')}
                   value=""
                 />
               ) : (
                 <SearchIcon />
-              )}
+              )} */}
               <ButtonWallet
                 src={ensAvatarUrl || address}
                 onClick={handleWalletButtonClick}
@@ -85,8 +86,8 @@ const ActionsWrapper = styled.div.attrs({
   className: 'flex space-x-3 items-center',
 })``;
 
-const SearchIcon = styled(IconSearch).attrs({
-  className: 'text-ui-0 h-2',
-})``;
+// const SearchIcon = styled(IconSearch).attrs({
+//   className: 'text-ui-0 h-2',
+// })``;
 
 export default ExploreNav;
